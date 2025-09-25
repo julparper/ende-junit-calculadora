d0c43b6b6c8202fbbe7400d86edc7ac2e31e87ff
Julian <julian@yo.com>
2025-09-25 13:38:26 +0200

    añadido spy con cambios

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Este es un ejemplo sencillo de pruebas unitarias usando Junit 5
 Y ejemplo de Mock usando Mockito. 
 > Observa que los DAO son interfaces y que ClienteDAOImpl **no** está implementado.
 
-En la clase MartingTest2 tienes un ejemplo de el uso parcial de Mocks (spy)
+En la clase MartingTest2 tienes un ejemplo de el uso parcial de Mocks (spy):
+- `contratarReal`: Ejecuta el método insertar de la clase OfertaDAOImpl
+- `contratarRealCambiado`: Ejecuta una simulación de insertar devolviendo 99 cuando se llame con María García.
--- src/test/java/es/etg/dax/testing/database/MarketingTest2.java
+++ src/test/java/es/etg/dax/testing/database/MarketingTest2.java
@@ -2,6 +2,7 @@ package es.etg.dax.testing.database;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,13 +35,30 @@ public class MarketingTest2 {
     OfertaDAO ofertaDAO = new OfertaDAOImpl(); //Objeto real, lo espiamos
 
     
-    //Ahora realizamos una prueba de forma convencional
+    //Ahora realizamos una prueba de forma convencional usandola implemtación real de OfertaDAO (OfertaDAOImpl)
     @Test
-    public void contratar() throws Exception{
+    public void contratarReal() throws Exception{
         
         //Ahora le decimos a Mockito que tiene que devolver en el caso de que llamen a cada mock. En este caso le decimos que cuando llamen a cada mock, devuelva 1
         Mockito.when(clientesDAO.insert(any())).thenReturn(1);
-        //Mockito.when(ofertaDAO.insertar(any())).thenReturn();
+
+        //Llamamos a la clase que realiza la lógica compleja:
+        boolean resultado = marketing.contratar("María", "García");
+
+        //Comprobamos que ha ido bien (true)
+        assertTrue(resultado);
+
+    }
+
+
+        @Test
+    public void contratarRealCambiado() throws Exception{
+        
+        //Ahora le decimos a Mockito que tiene que devolver en el caso de que llamen a cada mock. En este caso le decimos que cuando llamen a cada mock, devuelva 1
+        Mockito.when(clientesDAO.insert(any())).thenReturn(1);
+
+        Cliente cliente = new Cliente ("María", "García"); 
+        doReturn(99).when(ofertaDAO).insertar(cliente); // Cuando se llame a insertar de OfertaDAOImpl con Maria Garcia, devolverá 99
 
         //Llamamos a la clase que realiza la lógica compleja:
         boolean resultado = marketing.contratar("María", "García");
